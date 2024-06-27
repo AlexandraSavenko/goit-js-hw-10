4277f18d6a6f5579c16cb683725490f4330d7458
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-27 12:35:30 +0300

    dates get compared

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    compareDates(selectedDates[0]);
+  },
+};
+flatpickr('#datetime-picker', options);
+
+function compareDates(selectedDates) {
+  const currentDate = new Date();
+  if (selectedDates < currentDate) {
+    alert('Please choose a date in the future');
+  }
+}
+let userSelectedDate = 0;
+const elements = {
+  day: document.querySelector('[ data-days]'),
+  hour: document.querySelector('[data-hours]'),
+  minute: document.querySelector('[data-minutes]'),
+  second: document.querySelector('[data-seconds]'),
+};
