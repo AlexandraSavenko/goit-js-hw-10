44182d8220d580f1577523923456b0095d9ea741
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-27 17:01:30 +0300

    doing well with task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,23 @@
-console.log('Hello you!');
+console.log('you can do it!');
+const form = document.querySelector('.form');
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  const formInside = event.target;
+  const delay = event.target.elements.delay.value;
+  const stateF = event.target.elements.state.value;
+  //   const stateR = event.target.elements.state.value;
+  console.dir(formInside);
+  console.log(delay);
+  console.log(stateF);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateF === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+  promise.then().catch();
+}
