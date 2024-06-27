2ead04721a9404a153c4ac0175074eb2e9c7091d
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-27 17:16:34 +0300

    before easyToast

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,18 +6,17 @@ function handlerSubmit(event) {
   const formInside = event.target;
   const delay = event.target.elements.delay.value;
   const stateF = event.target.elements.state.value;
-  //   const stateR = event.target.elements.state.value;
   console.dir(formInside);
   console.log(delay);
   console.log(stateF);
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (stateF === 'fulfilled') {
-        resolve();
+        resolve(`Fulfilled promise in ${delay}ms`);
       } else {
-        reject();
+        reject(`Rejected promise in ${delay}ms`);
       }
     }, delay);
   });
-  promise.then().catch();
+  promise.then(value => alert(value)).catch(value => alert(value));
 }
